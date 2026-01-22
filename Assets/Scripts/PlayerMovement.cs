682b4f170b8fa8c75a8b95a890bfebfe343901a9
HelenaFernGo <helenafernandezg@icloud.com>
2026-01-22 20:17:38 +0100

    Particle System

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,10 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Rigidbody2D Rb;
-
     public float MoveSpeed;
-
     private Vector2 moveDirection;
-
     public InputActionReference Move;
+    public ParticleSystem GrassParticles;
 
     private void Update()
     {
@@ -20,5 +18,6 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         Rb.linearVelocity = new Vector2(x:moveDirection.x * MoveSpeed,y:moveDirection.y * MoveSpeed);
+        GrassParticles.Play();
     }
 }
