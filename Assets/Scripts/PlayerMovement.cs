6c4e877f5a717284fdf363816285e27cd62c434e
HelenaFernGo <helenafernandezg@icloud.com>
2026-01-22 19:41:36 +0100

    Player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Rigidbody2D Rb;
+
+    public float MoveSpeed;
+
+    private Vector2 moveDirection;
+
+    public InputActionReference Move;
+
+    private void Update()
     {
-        
+        moveDirection = Move.action.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Rb.linearVelocity = new Vector2(x:moveDirection.x * MoveSpeed,y:moveDirection.y * MoveSpeed);
     }
 }
