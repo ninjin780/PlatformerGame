ac3395336b782219ed20e3b8b6c73b0b3362e001
ninjin780 <ninjin780@gmail.com>
2026-01-22 18:30:02 +0100

    Conventions

--- Assets/Scripts/PoolManager.cs
+++ Assets/Scripts/PoolManager.cs
@@ -1,6 +1,20 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PoolManager : MonoBehaviour
 {
-    
+    private List<Coin> coins;
+
+    private void Awake()
+    {
+        coins = new List<Coin>();
+        initCoins();
+    }
+    private void initCoins()
+    {
+        for (int i = 0; i<5;i++)
+        {
+        }
+    }
 }
