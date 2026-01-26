53d2a18c01fcc1ac3e588ca15ea910a55bc877f0
NilBrillarelliiIcra <nilbrillaicra@gmail.com>
2026-01-26 20:40:59 +0100

    New Scene

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -5,6 +5,8 @@ public class Coin : MonoBehaviour
 {
     public static Action<Coin> OnCoinCollected;
 
+    public int Value;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PoolManager.BackToPool(gameObject);
