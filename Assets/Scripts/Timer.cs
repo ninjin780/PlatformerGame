c7d0d9c5757ef2f392d648a19a3614c5581aa0ed
Jon <jrovirasc@edu.tecnocampus.cat>
2026-01-27 16:45:35 +0100

    Update Timer.cs

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,44 +1,16 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    [Header("Timer Settings")]
-    public float startTime = 60f;
-
-    [Header("UI")]
-    public Text timeText;   
-
-    private float currentTime;
-    private bool isRunning = true;
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentTime = startTime;
-        UpdateTimerText();
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (!isRunning)
-            return;
-
-        currentTime -= Time.deltaTime;
-
-        if (currentTime <= 0f)
-        {
-            currentTime = 0f;
-            isRunning = false;
-        }
-
-        UpdateTimerText();
-    }
-
-    private void UpdateTimerText()
-    {
-        int seconds = Mathf.CeilToInt(currentTime);
-        timeText.text = "Time left:\n" + seconds;
+        
     }
 }
-
-
